34343153aa4bfcb4e84d4faaf8b842c1daacd64f
Alex Ignatovich <a.ignatovich92@yandex.by>
2022-02-22 10:26:49 +0300

    Main first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-app';
+
+    title: string = 'Dynamic title'
+    number: number = 42
+    arr: number[] = [1, 2, 3, 4, 5]
+
+    obj = { a: 1, b: {c: 2} }
+
 }
---
+++ src/app/post4/post4.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-post4',
+    template: `
+        <h1>Component</h1>
+    `,
+    styles: [`
+        h1 {color: red;}
+        `]
+})
+
+export class Post4Component {
+
+}

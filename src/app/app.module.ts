037daad966c07d9b7e03ca50e72aab05b012700f
alex_ignatovich <sasha_2804@mail.ru>
2022-03-28 17:16:24 +0300

    New learn animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,30 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-import { ModalComponent } from './modal/modal.component';
-import { RefDirective } from './ref.directive';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ModalComponent,
-    RefDirective
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: environment.production,
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    }),
+    BrowserAnimationsModule
   ],
   providers: [],
-  entryComponents: [ModalComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {
